1573f0f38e6454ead02f0f8cb8a165916a54f374
西野 <nisino333@gmail.com>
2019-08-24 20:17:44 +0900

    最初のコミット

---
+++ src/main/java/com/example/demo/UdemySampleProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySampleProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySampleProject2Application.class, args);
+	}
+
+}
